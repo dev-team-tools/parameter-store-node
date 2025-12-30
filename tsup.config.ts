28f3b801031e4bbe7664e7a666efd9a7fe3f149d
Harry Winser <harry@harrywinser.com>
2025-12-30 10:27:59 +0000

    Fix issue where it wasn't building the library correctly

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,9 +4,9 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
-  dts: true,
-  clean: true,
   outExtension({ format }) {
     return { js: format === "cjs" ? ".cjs" : ".mjs" };
   },
+  dts: true,
+  clean: true,
 });
